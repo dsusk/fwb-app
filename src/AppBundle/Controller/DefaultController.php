3e447dbfaf645d65aa6e35c972aa73678e6e6b7c
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-03-14 14:47:24 +0100

    Add basic layout

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,26 +3,93 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Solarium\QueryType\Select\Query\FilterQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+
+    /**
+     * @var \Solarium\Core\Client\Client
+     */
+    protected $client;
+
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
 
-
-        $client = $this->get('solarium.client');
-        $select = $client->createSelect();
-        $select->setQuery('foo');
-        $results = $client->select($select);
+        $this->client = $this->get('solarium.client');
 
         // replace this example code with whatever you need
         return $this->render('search/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
+
+    /**
+     * @Route("/search", name="search")
+     *
+     */
+    public function searchAction()
+    {
+
+        $this->client = $this->get('solarium.client');
+
+        $request = Request::createFromGlobals();
+
+        $client = $this->client;
+
+        $searchTerm = 'lemma:' . $request->get('q') . '*';
+
+        // todo move into config
+        $rows = 20;
+        $currentPage = (int)$request->get('page') ?: 1;
+
+        $offset = ($currentPage - 1) * $rows;
+
+        $query = $client->createSelect();
+
+        $query->setStart($offset);
+        $query->setRows($rows);
+
+        $query->setQuery($searchTerm);
+
+        // get the facetset component
+        $facetSet = $query->getFacetSet();
+
+        $facetSet->createFacetField('type_of_word');
+
+        $fq = new FilterQuery();
+        $fq->setKey('type');
+        $fq->setQuery('type:artikel');
+        $query->addFilterQuery($fq);
+        $resultset = $client->select($query);
+
+
+        $facets = $resultset->getFacetSet()->getFacet('type_of_word');
+
+        /*
+        $results = new LengthAwarePaginator($resultset->getDocuments(), $resultset->getNumFound(), $rows, null, ['path' => 'search']);
+
+        return view('search.results',
+            [
+                'searchTerm' => $request->get('q'),
+                'resultCount' => $resultset->getNumFound(),
+                'results' => $results,
+                'firstItem' => $results->firstItem(),
+                'facets' => $facets
+            ]
+
+        */
+        return $this->render('search/results.html.twig', [
+                    'searchTerm' => $request->get('q'),
+            'resultCount' => $resultset->getNumFound()
+                ]);
+
+    }
+
 }
